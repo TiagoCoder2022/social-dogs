324c691b510893dd9f7d3d31099b1b2157a1ec1a
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-06 01:38:30 -0300

    creating our modal photo

--- src/components/Feed/FeedModal.jsx
+++ src/components/Feed/FeedModal.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import styles from './FeedModal.module.css'
+import useFetch from '../../Hooks/useFetch'
+import Error from '../Helper/Error'
+import Loading from '../Helper/Loading'
+import { PHOTO_GET } from '../../api'
 
-const FeedModal = () => {
+const FeedModal = ({photo}) => {
+  const {data, error, loading, request} = useFetch()
+
+  function handleOutsideClick(event) {
+    
+  }
+
+  useEffect(() => {
+    const {url,options} = PHOTO_GET(photo.id)
+    request(url, options)
+  }, [photo, request])
   return (
-    <div>FeedModal</div>
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error}/>}
+      {loading && <Loading/>}
+      {data && <PhotoContent data={data}/>}
+    </div>
   )
 }
 
