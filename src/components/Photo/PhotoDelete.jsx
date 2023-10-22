6488e4baaa3dbce3965481a5212d4b035be0174c
TiagoCoder2022 <tiagonewalves@hotmail.com>
2023-10-22 20:15:22 -0300

    fix: add the navigate to photo delete function

--- src/components/Photo/PhotoDelete.jsx
+++ src/components/Photo/PhotoDelete.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
-import styles from './PhotoDelete.module.css'
-import { PHOTO_DELETE } from '../../api'
-import useFetch from '../../Hooks/useFetch'
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+import { PHOTO_DELETE } from "../../api";
+import useFetch from "../../Hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const PhotoDelete = ({ id }) => {
-  const { loading, request } = useFetch()
+  const { loading, request } = useFetch();
+  const navigate = useNavigate();
 
-  async function handleDelete() { 
-    const confirm = window.confirm('Tem certeza que deseja deletar?')   
+  async function handleDelete() {
+    const confirm = window.confirm("Tem certeza que deseja deletar?");
     if (confirm) {
-      const { url, options } = PHOTO_DELETE(id)
-      const { response } = await request(url, options)
-      if(response.ok) window.location.reload()
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+
+      if (response.ok) {
+        window.location.reload();
+        navigate("/conta");
+      }
     }
   }
 
   return (
     <>
       {loading ? (
-        <button className={styles.delete} disabled >
+        <button className={styles.delete} disabled>
           Deletar
         </button>
       ) : (
         <button onClick={handleDelete} className={styles.delete}>
           Deletar
         </button>
-      )}      
+      )}
     </>
-  )
-}
+  );
+};
 
-export default PhotoDelete
+export default PhotoDelete;
--- src/components/User/UserHeaderNav.jsx
+++ src/components/User/UserHeaderNav.jsx
@@ -1,60 +1,64 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { NavLink, useLocation, useNavigate } from 'react-router-dom'
-import { UserContext } from '../../UserContext'
-import MinhasFotos from "../../Assets/feed.svg?react"
-import Estatisticas from "../../Assets/estatisticas.svg?react"
-import AdicionarFotos from "../../Assets/adicionar.svg?react"
-import Sair from "../../Assets/sair.svg?react"
-import styles from './UserHeaderNav.module.css'
-import useMedia from '../../Hooks/useMedia'
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { UserContext } from "../../UserContext";
+import MinhasFotos from "../../Assets/feed.svg?react";
+import Estatisticas from "../../Assets/estatisticas.svg?react";
+import AdicionarFotos from "../../Assets/adicionar.svg?react";
+import Sair from "../../Assets/sair.svg?react";
+import styles from "./UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
 
+const UserHeaderNav = () => {
+  const { userLogout } = useContext(UserContext);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const navigate = useNavigate();
 
-const UserHeaderNav = () => {  
-  const { userLogout } = useContext(UserContext)
-  const mobile = useMedia('(max-width: 40rem)')
-  const [mobileMenu, setMobileMenu] = useState(false)
-  const navigate = useNavigate()
-
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   useEffect(() => {
-    setMobileMenu(false)
-  }, [pathname])
- 
+    setMobileMenu(false);
+  }, [pathname]);
 
   function handleLogout() {
-    userLogout()
-    navigate('/login')
+    userLogout();
+    navigate("/login");
   }
 
   return (
     <>
       {mobile && (
-        <button 
-          aria-label="Menu" 
-          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+        <button
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
           onClick={() => setMobileMenu(!mobileMenu)}
         ></button>
       )}
-      <nav className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}>
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
         <NavLink to="/conta" end>
           <MinhasFotos />
-          {mobile && 'Minhas Fotos'}
+          {mobile && "Minhas Fotos"}
         </NavLink>
         <NavLink to="/conta/estatisticas">
-          <Estatisticas/>
-          {mobile && 'Estatísticas'}        
+          <Estatisticas />
+          {mobile && "Estatísticas"}
         </NavLink>
         <NavLink to="/conta/postar">
-          <AdicionarFotos/>
-          {mobile && 'Adicionar'}        
+          <AdicionarFotos />
+          {mobile && "Adicionar"}
         </NavLink>
         <button onClick={handleLogout}>
           <Sair />
-          {mobile && 'Sair'}
+          {mobile && "Sair"}
         </button>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default UserHeaderNav
+export default UserHeaderNav;
